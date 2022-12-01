730c65fecf2d2817faf67fa065b1401cb1245f30
fabi2k03 <fabian.kopf03@gmail.com>
2022-12-01 10:00:40 +0100

    Detail Page Changes v1

--- webpack-immer/webpack/src/components/driver-component.ts
+++ webpack-immer/webpack/src/components/driver-component.ts
@@ -1,29 +1,49 @@
 import { html, render } from "lit-html"
+import { Driver } from "../model/driver"
 
-const template = html`
-    <div>Driver: Max Mustermann</div>
-`
+const driverComponentTemplate = (driver: Driver) => html`
+    <button id="backbutton">Back</button>
+    <div>Number: ${driver.driverNumber}</div>
+    <div>Name: ${driver.nameDriver}</div>
+    <div>Team: ${driver.team}</div>
+    <div>Nation: ${driver.nation}</div>`
 
 class DriverComponent extends HTMLElement {
     static get observedAttributes() {
         return ["driverNumber"]
     }
+
+    driverNumber: Driver
+
+    private root: ShadowRoot
+
     constructor() {
         super()
         this.attachShadow({mode: "open"})
     }
-    attributeChangedCallback(name: string, oldValue: string, value: string) {
-        console.log("TODO: display driver", value)
+
+    attributeChangedCallback(name: string, oldValue: string, value: Driver) {
+        this.driverNumber = value
+        console.log("Driver is here", this.driverNumber)
+        this.render()
+
     }
     connectedCallback() {
-        console.log("driver connected")
-        this.render()
+        console.log("driver connected in driver component")
+        //this.render()
     }
     private render() {
-        render(template, this.shadowRoot)
+        let driver: Driver
+        console.log("IM here")
+        driver = this.driverNumber
+        render(driverComponentTemplate(driver), this.root)
+        this.root.getElementById("backbutton").onclick = () => {
+            const event = new CustomEvent("back")
+            this.dispatchEvent(event)
+        }
+        
     }
 
 
 }
-
 customElements.define("driver-component", DriverComponent)
